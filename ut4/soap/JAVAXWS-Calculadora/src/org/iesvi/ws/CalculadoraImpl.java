eaa6030cf40507cc4c2d6bde427ec77f6ca7a424
AngelusWoo <angelwoocastillo@gmail.com>
2022-10-24 08:27:27 +0200

    calculadora

---
+++ ut4/soap/JAVAXWS-Calculadora/src/org/iesvi/ws/CalculadoraImpl.java
@@ -0,0 +1,19 @@
+package org.iesvi.ws;
+
+import javax.jws.WebService;
+
+public class CalculadoraImpl implements Calculadora{
+
+    @Override
+    public double operacion(int opcion, int valor1, int valor2) {
+        double resultado = 0;
+        switch (opcion) {
+            case 1: resultado = valor1 + valor2; break;
+            case 2: resultado = valor1 - valor2; break;
+            case 3: resultado = valor1 * valor2; break;
+            case 4: resultado = valor1 / valor2; break;
+            default: break;
+        }
+        return resultado;
+    }
+}
