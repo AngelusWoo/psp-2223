f357d64c164be43766c33bcf51c5299c455cdca8
AngelusWoo <angelwoocastillo@gmail.com>
2022-10-24 08:27:05 +0200

    calculadora

---
+++ ut4/soap/JAVAXWS-Calculadora/src/org/iesvi/ws/Calculadora.java
@@ -0,0 +1,9 @@
+package org.iesvi.ws;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+public interface Calculadora {
+    @WebMethod
+    public double operacion(int opcion, int valor1, int valor2);
+}
