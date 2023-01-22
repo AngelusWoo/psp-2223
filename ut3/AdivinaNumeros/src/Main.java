99304b20ee2986af4309cac1f96f2e83e5cd1c39
Angel Woo <angelwoocastillo@gmail.com>
2023-01-22 20:29:43 +0100

    Hilos Compartiendo Objetos

---
+++ ut3/AdivinaNumeros/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Arbitro arbitro = new Arbitro(3,1);
+        Jugador j1 = new Jugador(1,arbitro);
+        Jugador j2 = new Jugador(2,arbitro);
+        Jugador j3 = new Jugador(3,arbitro);
+
+        System.out.println("NUMERO A ADIVINAR: " + arbitro.getNumero());
+
+        j1.start();
+        j2.start();
+        j3.start();
+    }
+}
