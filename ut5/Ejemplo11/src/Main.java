680f26a892e0229d5831cb0b855b63fa169b5fb0
AngelusWoo <angelwoocastillo@gmail.com>
2023-02-20 09:29:24 +0100

    Updated Ejemplo11

--- ut5/Ejemplo11/src/Main.java
+++ ut5/Ejemplo11/src/Main.java
@@ -16,9 +16,12 @@ public class Main {
             Cipher c = Cipher.getInstance("AES/ECB/PKCS5Padding");
             c.init(Cipher.ENCRYPT_MODE,clavepub);
 
-            byte textoPlano[] = "Esto es un texto Plano".getBytes();
-            byte textoCifrado[] = c.doFinal(textoPlano);
-            System.out.println("Encriptado: " + new String(textoCifrado));
+            byte[] textoPlano = "Esto es un texto Plano".getBytes();
+            byte[] textoCifrado = c.doFinal(textoPlano);
+
+            c.init(Cipher.DECRYPT_MODE,clavepriv);
+            byte desencriptado[] = c.doFinal(textoCifrado);
+            System.out.println("Desencriptado: " + new String(desencriptado));
 
         } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException |
                  BadPaddingException e) {
