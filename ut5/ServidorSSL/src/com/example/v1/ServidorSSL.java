71bad81ff94a388ca8c4562cecda27532b6c6157
AngelusWoo <angelwoocastillo@gmail.com>
2023-03-02 14:37:47 +0100

    Separated v1 and v2. Fixed commands description

---
+++ ut5/ServidorSSL/src/com/example/v1/ServidorSSL.java
@@ -0,0 +1,37 @@
+package com.example.v1;
+
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
+import javax.net.ssl.SSLSocket;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class ServidorSSL {
+    public static void main(String[] args) throws IOException {
+        int puerto = 6000;
+
+        SSLServerSocketFactory sfact = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
+        SSLServerSocket servidorSSL = (SSLServerSocket) sfact.createServerSocket(puerto);
+        SSLSocket clienteConectado = null;
+        DataInputStream flujoEntrada = null;
+        DataOutputStream flujoSalida = null;
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Esperando al cliente " + i);
+            clienteConectado = (SSLSocket) servidorSSL.accept();
+            flujoEntrada = new DataInputStream(clienteConectado.getInputStream());
+
+            System.out.println("Recibiendo del CLIENTE: " + i + " \n\t" + flujoEntrada.readUTF());
+
+            flujoSalida = new DataOutputStream(clienteConectado.getOutputStream());
+
+            flujoSalida.writeUTF("Saludos al cliente del servidor");
+        }
+
+        flujoEntrada.close();
+        flujoSalida.close();
+        clienteConectado.close();
+        servidorSSL.close();
+    }
+}
